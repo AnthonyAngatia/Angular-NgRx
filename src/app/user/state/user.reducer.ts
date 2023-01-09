2574f9de0b58361283b34ebdb40ba98ec5d7c88a
anthony angatia <anthonyangatia@gmail.com>
2023-01-09 12:37:30 +0300

    [Fix]Fixed bug in user reducer

--- src/app/user/state/user.reducer.ts
+++ src/app/user/state/user.reducer.ts
@@ -1,7 +1,7 @@
 import {createAction, createReducer, on} from '@ngrx/store';
 
 export const userReducer = createReducer(
-  {maskUser: false}, on(createAction('Mask user', state => {
+  {maskUser: false}, on(createAction('Mask user'), state => {
     console.log('original state ' + JSON.stringify(state));
     return {...state, maskUser: !state.maskUser};
-  })));
+  }));
